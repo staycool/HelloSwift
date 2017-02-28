d7362f346f5eb6338f2adcbe8086d4ac8465e8a7
staycool <zhubinvictory@hotmail.com>
2017-02-28 13:44:30 +0800

    A little test
    Do some changes  by new knowledge learned from tutorial books.

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var TextView1: UITextView!
+    
+    var flag:int_fast16_t = -1
+    @IBAction func onClick(sender: AnyObject) {
+        flag = flag + 1
+        switch flag % 3 {
+        case 0 :
+            self.Label1.text = "Hello Swift!"
+        case 1 :
+            self.Label2.text = "Author:zb"
+        case 2 :
+            self.TextView1.text = "ViewController.swift"
+            + "HelloSwift"
+            + "Created by Zhu's Mac on 16/7/6."
+            + "Copyright © 2016年 Zhu's Mac. All rights reserved."
+        default : break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
